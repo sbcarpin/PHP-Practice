8fcbef8f77c543eac944ac5d6577ec6e7e318b09
Stephanie <stephcarpin710@gmail.com>
2019-04-20 16:58:05 -0700

    while

---
+++ php-basics/whileloop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+ 
+<html>
+    <head>
+        <title>While Loops</title>
+    </head>
+    <body>
+         
+<?php
+    $p = 1;
+    //will print out for 10 times (counting up)
+    while($p <= 10){
+        echo "{$p} - steph is cool <br>";
+        $p++;
+    }    
+?>
+    </body>
+</html>
